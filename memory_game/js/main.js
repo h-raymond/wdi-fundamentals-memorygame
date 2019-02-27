513bf35a60174a4a967807b93cfc50786dc41fba
Hugo Raymond <hugoraymond@Hugos-iMac.home>
2019-02-27 20:22:51 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match");
+} else {
+	alert("Sorry try again");
+}
