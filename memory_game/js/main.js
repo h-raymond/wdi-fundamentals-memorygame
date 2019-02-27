6884a1d68c3ed3e66396764de6b2c0cf1945009b
h-raymond <hugo.raymond@gmail.com>
2019-02-27 22:22:22 +0000

    created game logic: updated arrays and additional functions to cover cardImage and suit

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+},
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,12 +35,18 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-	console.log("User flipped " + cards[cardId])
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	
-	cardsInPlay.push(cards[cardId]);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].cardImage);
+	
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].suit);
 
 	checkForMatch();
 }
+
 
 flipCard(0);
 flipCard(2);
